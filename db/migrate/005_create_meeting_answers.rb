50d925d417eb5ba628412fce78fc4e116d5a90ef
rubynovich <roman@shipiev.me>
2013-08-22 16:00:22 +0400

    add: to scheme meeting_answers 3 fields

--- db/migrate/005_create_meeting_answers.rb
+++ db/migrate/005_create_meeting_answers.rb
@@ -4,14 +4,18 @@ class CreateMeetingAnswers < ActiveRecord::Migration
       t.references :meeting_protocol
       t.references :meeting_question
       t.text :description
+      t.integer :reporter_id
       t.references :user
       t.references :issue
+      t.date :start_date
+      t.date :due_date
       t.datetime :created_on
       t.datetime :updated_on
     end
     add_index :meeting_answers, :meeting_question_id
     add_index :meeting_answers, :meeting_protocol_id
     add_index :meeting_answers, :user_id
+    add_index :meeting_answers, :reporter_id
     add_index :meeting_answers, :issue_id
   end
 end
