72037800950c90e7fc09eb1f51ad9ada8e88ddd0
rubynovich <roman@shipiev.me>
2013-08-23 16:07:37 +0400

    ops

--- lib/time_period_scope.rb
+++ lib/time_period_scope.rb
@@ -18,26 +18,31 @@ module TimePeriodScope
       scope :time_period, lambda {|q, field|
         today = Date.today
         if q.present?
-          conditions = case q
-                         when "yesterday"
-                           {field => 1.day.ago}
-                         when "today"
-                           {field => today}
-                         when "last_week"
-                           ["#{field} BETWEEN ? AND ?", 1.week.ago.beginning_of_week, 1.week.ago.end_of_week ]
-                         when "this_week"
-                           ["#{field} BETWEEN ? AND ?", today.beginning_of_week, today.end_of_week]
-                         when "last_month"
-                           ["#{field} BETWEEN ? AND ?", 1.month.ago.beginning_of_month, 1.month.ago.end_of_month]
-                         when "this_month"
-                           ["#{field} BETWEEN ? AND ?", today.beginning_of_month, today.end_of_month]
-                         when "last_year"
-                           ["#{field} BETWEEN ? AND ?", 1.year.ago.beginning_of_year, 1.year.ago.end_of_year ]
-                         when "this_year"
-                           ["#{field} BETWEEN ? AND ?", today.beginning_of_year, today.end_of_year ]
-                         when "any"
-                         end
+          period_start, period_end = case q
+                                     when "yesterday"
+                                       [1.day.ago.beginning_of_day, 1.day.ago.end_of_day]
+                                     when "today"
+                                       [today.beginning_of_day, today.end_of_day]
+                                     when "last_week"
+                                       [1.week.ago.beginning_of_week, 1.week.ago.end_of_week]
+                                     when "this_week"
+                                       [ today.beginning_of_week, today.end_of_week]
+                                     when "last_month"
+                                       [1.month.ago.beginning_of_month, 1.month.ago.end_of_month]
+                                     when "this_month"
+                                       [today.beginning_of_month, today.end_of_month]
+                                     when "last_year"
+                                       [1.year.ago.beginning_of_year, 1.year.ago.end_of_year ]
+                                     when "this_year"
+                                       [today.beginning_of_year, today.end_of_year ]
+                                     when "any"
+                                     end
+          if field.present? && period_start.present?
+            conditions = ["#{field} BETWEEN ? AND ?", period_start , period_end]
+          end
+
           {:conditions => conditions}
+
         end
       }
 
