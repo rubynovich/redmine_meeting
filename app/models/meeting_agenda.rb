b213b92b0624824eac55f211af4f383481ec7188
rubynovich <roman@shipiev.me>
2013-08-26 10:15:05 +0400

    ops

--- app/models/meeting_agenda.rb
+++ app/models/meeting_agenda.rb
@@ -21,7 +21,7 @@ class MeetingAgenda < ActiveRecord::Base
 
   validates_uniqueness_of :subject, scope: :meet_on
   validates_presence_of :subject, :place, :meet_on, :start_time, :end_time
-  validate :start_time_less_than_end_time, if: -> {self.start_time > self.end_time}
+  validate :start_time_less_than_end_time, if: -> {self.start_time >= self.end_time}
   validate :presence_of_meeting_questions, if: -> {self.meeting_questions.blank?}
   validate :presence_of_meeting_members, if: -> {self.meeting_members.blank?}
 
