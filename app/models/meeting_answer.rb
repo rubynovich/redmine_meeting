9d48377f5243ecc3db0025dfe7391a46e86cefca
rubynovich <roman@shipiev.me>
2013-08-28 14:50:46 +0400

    fix: protocols#create and #new

--- app/models/meeting_answer.rb
+++ app/models/meeting_answer.rb
@@ -8,6 +8,18 @@ class MeetingAnswer < ActiveRecord::Base
   belongs_to :issue
   has_one :status, through: :issue
   has_one :project, through: :issue
+  has_many :meeting_answers, through: :meeting_protocol
 
   validates_presence_of :user_id, :description, :start_date, :due_date, :meeting_question_id
+
+  after_save :add_new_users_from_answers
+
+private
+
+  def add_new_users_from_answers
+    (self.meeting_answers.map(&:reporter) - self.meeting_protocol.users).each do |user|
+      MeetingParticipator.create(user: user, meeting_protocol: self.meeting_protocol)
+    end
+  end
+
 end
