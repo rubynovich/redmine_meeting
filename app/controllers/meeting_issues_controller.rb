2f613dc4313a5c2a6e045ab440d18a776c718e48
rubynovich <roman@shipiev.me>
2013-08-29 15:47:08 +0400

    add: meeting_issues#create

--- app/controllers/meeting_issues_controller.rb
+++ app/controllers/meeting_issues_controller.rb
@@ -1,10 +1,10 @@
 class MeetingIssuesController < ApplicationController
   unloadable
 
-  before_filter :find_object, only: [:new, :create]
+  before_filter :find_object, only: [:new, :create, :destroy]
+  before_filter :new_issue, only: [:new, :create]
 
   def new
-    @issue = Issue.new
     @issue.assigned_to = @object.user
     @issue.start_date = @object.start_date
     @issue.due_date = @object.due_date
@@ -12,7 +12,22 @@ class MeetingIssuesController < ApplicationController
     @issue.description = @object.description
   end
 
+  def create
+    if @issue.save
+      @object.update_attribute(:issue_id, @issue.id)
+    else
+      render action: :new
+    end
+  end
+
+  def destroy
+    @object.update_attribute(:issue_id, nil)
+  end
+
 private
+  def new_issue
+    @issue = Issue.new(params[:issue])
+  end
 
   def find_object
     @object = MeetingAnswer.find(params[:meeting_answer_id])
