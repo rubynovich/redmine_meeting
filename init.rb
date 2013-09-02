f6c64709e174dc78b246b8335a00e4170c46cf6d
rubynovich <roman@shipiev.me>
2013-09-02 13:47:14 +0400

    add: application_menu

--- init.rb
+++ init.rb
@@ -14,6 +14,32 @@ Redmine::Plugin.register :redmine_meeting do
     description: "%url%",
     note: "%question%\n\n%description%\n\n%url%"
   }
+
+  permission :view_meeting_agendas, meeting_agendas: [:index, :show]
+  permission :add_meeting_agendas, meeting_agendas: [:new, :create]
+  permission :edit_meeting_agendas, meeting_agendas: [:edit, :update]
+  permission :delete_meeting_agendas, meeting_agendas: [:destroy]
+
+  permission :view_meeting_protocols, meeting_protocols: [:index, :show]
+  permission :add_meeting_protocols, meeting_protocols: [:new, :create]
+  permission :edit_meeting_protocols, meeting_protocols: [:edit, :update]
+  permission :delete_meeting_protocols, meeting_protocols: [:destroy]
+
+  menu :application_menu, :meeting_agendas,
+    {controller: 'meeting_agendas', action: 'index'},
+    caption: :label_meeting_agenda_plural,
+    param: 'project_id',
+    if: Proc.new{
+      User.current.allowed_to?({controller: 'meeting_agendas', action: 'index'}, nil, {global: true})
+    }
+
+  menu :application_menu, :meeting_protocols,
+    {controller: 'meeting_protocols', action: 'index'},
+    caption: :label_meeting_protocol_plural,
+    param: 'project_id',
+    if: Proc.new{
+      User.current.allowed_to?({controller: 'meeting_protocols', action: 'index'}, nil, {global: true})
+    }
 end
 
 Rails.configuration.to_prepare do
