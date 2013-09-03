56ceca6669b35ab92da99ef5629d8e1f0fab960d
rubynovich <roman@shipiev.me>
2013-09-03 10:38:51 +0400

    fix: meeting_agenda validation

--- app/models/meeting_agenda.rb
+++ app/models/meeting_agenda.rb
@@ -23,7 +23,10 @@ class MeetingAgenda < ActiveRecord::Base
   validates_uniqueness_of :subject, scope: :meet_on
   validates_presence_of :subject, :place, :meet_on, :start_time, :end_time, :priority_id
   validate :end_time_less_than_start_time, if: -> {self.start_time && self.end_time && (self.end_time <= self.start_time)}
-  validate :meet_on_less_than_today, if: -> {self.meet_on && (self.meet_on < Date.today) && self.start_time && (self.start_time < Time.now)}
+  validate :meet_on_less_than_today, if: -> {
+    self.meet_on && (self.meet_on < Date.today) &&
+    self.start_time && (self.start_time.seconds_since_midnight < Time.now.seconds_since_midnight)
+  }
   validate :presence_of_meeting_questions, if: -> {self.meeting_questions.blank?}
   validate :presence_of_meeting_members, if: -> {self.meeting_members.blank?}
 
