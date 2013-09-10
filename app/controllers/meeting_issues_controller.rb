58bbe232618983116f69678004d3ec4923bf7885
rubynovich <roman@shipiev.me>
2013-09-10 15:21:00 +0400

    add: bind_issues

--- app/controllers/meeting_issues_controller.rb
+++ app/controllers/meeting_issues_controller.rb
@@ -15,10 +15,10 @@ class MeetingIssuesController < ApplicationController
 
   def create
     @issue.author_id = params[:issue][:author_id]
-    @issue.description += "\n\n" +
-      t(:message_description_protocol_information, url: url_for(controller: 'meeting_protocols', action: 'show', id: @object.meeting_protocol_id))
     @issue.parent_issue_id = params[:issue][:parent_issue_id]
     if @issue.save
+      @issue.update_attribute(:description, @issue.description + "\n\n" +
+        t(:message_description_protocol_information, url: url_for(controller: 'meeting_protocols', action: 'show', id: @object.meeting_protocol_id))
       @object.update_attribute(:issue_id, @issue.id)
       @object.update_attribute(:issue_type, :new)
     else
