122408cbd29487b2a2a99cd9c078208419b3713d
rubynovich <roman@shipiev.me>
2013-09-17 13:06:59 +0400

    ops

--- db/migrate/20130917085526_copy_start_time_and_end_time_from_meeting_agenda_to_meeting_protocols.rb
+++ db/migrate/20130917085526_copy_start_time_and_end_time_from_meeting_agenda_to_meeting_protocols.rb
@@ -1,10 +1,10 @@
 class CopyStartTimeAndEndTimeFromMeetingAgendaToMeetingProtocols < ActiveRecord::Migration
   def up
     MeetingProtocol.where("start_time IS ?", nil).each do |protocol|
-      protocol.update_attribute(:start_time, protocol.meeting_agenda.start_time)
+      protocol.update_attribute(:start_time, protocol.meeting_agenda.start_time) if protocol.meeting_agenda.present?
     end
     MeetingProtocol.where("end_time IS ?", nil).each do |protocol|
-      protocol.update_attribute(:end_time, protocol.meeting_agenda.end_time)
+      protocol.update_attribute(:end_time, protocol.meeting_agenda.end_time) if protocol.meeting_agenda.present?
     end
   end
 
