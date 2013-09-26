494fcf9ad81d43867323b87650b64ba77af3b967
rubynovich <roman@shipiev.me>
2013-09-26 16:51:37 +0400

    add: permissions for watchers

--- app/helpers/meeting_watchers_helper.rb
+++ app/helpers/meeting_watchers_helper.rb
@@ -1,9 +1,9 @@
 module MeetingWatchersHelper
   def can_create_watcher?(item)
-    admin?(item) || author?(item)
+    admin? || author?(item)
   end
 
   def can_destroy_watcher?(item)
-    admin?(item) || author?(item)
+    admin? || author?(item)
   end
 end
