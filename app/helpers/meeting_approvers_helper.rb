22713ddcce612d4b2644094e7b6a6dc1595f49fc
rubynovich <roman@shipiev.me>
2013-09-28 17:39:14 +0400

    ops

--- app/helpers/meeting_approvers_helper.rb
+++ app/helpers/meeting_approvers_helper.rb
@@ -1,4 +1,12 @@
 module MeetingApproversHelper
+  def admin?
+    User.current.admin?
+  end
+
+  def author?(item)
+    User.current == item.author
+  end
+
   def can_create_approver?(item)
     author?(item) || admin?
   end
--- app/helpers/meeting_comments_helper.rb
+++ app/helpers/meeting_comments_helper.rb
@@ -1,4 +1,12 @@
 module MeetingCommentsHelper
+  def admin?
+    User.current.admin?
+  end
+
+  def author?(item)
+    User.current == item.author
+  end
+
   def can_show_comments?(container)
     admin? || meeting_manager? || container.users.include?(User.current)
   end
