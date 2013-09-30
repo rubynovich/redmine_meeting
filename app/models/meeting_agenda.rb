73e7fd05830027dcb31c799d3cc1e25919ef0c2a
rubynovich <roman@shipiev.me>
2013-09-30 15:32:14 +0400

    add: meeting_room_reserve

--- app/models/meeting_agenda.rb
+++ app/models/meeting_agenda.rb
@@ -111,7 +111,7 @@ private
   end
 
   def meeting_room_reserve_attributes
-    {user_id: User.current,
+    {user_id: User.current.id,
     subject: self.subject,
     meeting_room_id: find_meeting_room.id,
     reserve_on: self.meet_on,
