cf68d598e064806afad3c78c5255fd434b89df67
rubynovich <roman@shipiev.me>
2013-10-01 11:11:51 +0400

    ops

--- app/controllers/meeting_protocols_controller.rb
+++ app/controllers/meeting_protocols_controller.rb
@@ -55,7 +55,7 @@ class MeetingProtocolsController < ApplicationController
       uniq
 
     @scope = @scope.joins(:meeting_participators).joins(:meeting_answers).includes(:meeting_watchers).includes(:meeting_approvers).
-      where("meeting_protocols.author_id = :user_id OR meeting_participators.user_id = :user_id OR meeting_answers.reporter_id = :user_id", user_id: User.current.id) unless admin?
+      where("meeting_protocols.author_id = :user_id OR meeting_participators.user_id = :user_id OR meeting_answers.reporter_id = :user_id OR meeting_watchers.user_id = :user_id OR meeting_approvers.user_id = :user_id", user_id: User.current.id) unless admin?
 
     @count = @scope.count
 
