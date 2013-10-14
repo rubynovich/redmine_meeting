27f07528eec4a69fe8d4e9bdbe612c0bd3148454
rubynovich <roman@shipiev.me>
2013-10-14 16:06:33 +0400

    ops

--- app/models/meeting_protocol.rb
+++ app/models/meeting_protocol.rb
@@ -7,7 +7,7 @@ class MeetingProtocol < ActiveRecord::Base
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :meeting_agenda
   has_one  :external_company, through: :meeting_agenda
-  has_many :meeting_answers, dependent: :delete_all, order: [:meeting_question_id]
+  has_many :meeting_answers, dependent: :delete_all
   has_many :meeting_extra_answers, dependent: :delete_all
   has_many :issues, through: :meeting_answers, uniq: true
   has_many :meeting_participators, dependent: :delete_all
