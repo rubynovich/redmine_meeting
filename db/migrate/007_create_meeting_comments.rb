ef832e807d35c71604859bcf2e0abe74211cf294
rubynovich <roman@shipiev.me>
2013-10-18 16:09:55 +0400

    ops

--- db/migrate/007_create_meeting_comments.rb
+++ db/migrate/007_create_meeting_comments.rb
@@ -1,12 +1,12 @@
 class CreateMeetingComments < ActiveRecord::Migration
   def change
-    create_table :meeting_comments do |t|
-      t.integer :meeting_container_id, null: false
-      t.string  :meeting_container_type, null: false
-      t.references :author
-      t.text :note
-      t.datetime :created_on
-    end
-    add_index :meeting_comments, :author_id
+#    create_table :meeting_comments do |t|
+#      t.integer :meeting_container_id, null: false
+#      t.string  :meeting_container_type, null: false
+#      t.references :author
+#      t.text :note
+#      t.datetime :created_on
+#    end
+#    add_index :meeting_comments, :author_id
   end
 end
