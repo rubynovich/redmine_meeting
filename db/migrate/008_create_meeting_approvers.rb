781cda2254f149894bf5cdd328c9cbfa1e8aa3fb
rubynovich <roman@shipiev.me>
2013-10-18 16:12:26 +0400

    ops

--- db/migrate/008_create_meeting_approvers.rb
+++ db/migrate/008_create_meeting_approvers.rb
@@ -1,15 +1,15 @@
 class CreateMeetingApprovers < ActiveRecord::Migration
   def change
-#    create_table :meeting_approvers do |t|
-#      t.references :user
-#      t.string :meeting_container_type
-#      t.integer :meeting_container_id
-#      t.boolean :approved
-#      t.boolean :deleted, default: false
-#      t.integer :author_id
-#      t.datetime :approved_on
-#      t.datetime :created_on
-#    end
-#    add_index :meeting_approvers, :user_id
+    create_table :meeting_approvers do |t|
+      t.references :user
+      t.string :meeting_container_type
+      t.integer :meeting_container_id
+      t.boolean :approved
+      t.boolean :deleted, default: false
+      t.integer :author_id
+      t.datetime :approved_on
+      t.datetime :created_on
+    end
+    add_index :meeting_approvers, :user_id
   end
 end
--- db/migrate/011_create_meeting_watchers.rb
+++ db/migrate/011_create_meeting_watchers.rb
@@ -1,10 +1,10 @@
 class CreateMeetingWatchers < ActiveRecord::Migration
   def change
-#    create_table :meeting_watchers do |t|
-#      t.string :meeting_container_type
-#      t.integer :meeting_container_id
-#      t.integer :user_id
-#      t.datetime :created_on
-#    end
+    create_table :meeting_watchers do |t|
+      t.string :meeting_container_type
+      t.integer :meeting_container_id
+      t.integer :user_id
+      t.datetime :created_on
+    end
   end
 end
