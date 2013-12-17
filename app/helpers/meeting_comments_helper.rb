4e9278d4d57b36eabdbf79e99647bab28398b0b6
rubynovich <roman@shipiev.me>
2013-12-17 11:33:23 +0400

    add: watchers can add comments

--- app/helpers/meeting_comments_helper.rb
+++ app/helpers/meeting_comments_helper.rb
@@ -15,13 +15,13 @@ module MeetingCommentsHelper
     admin? ||
     meeting_manager? ||
     container.users.include?(User.current) ||
-    container.watchers.include?(User.current)
+    @object.watchers.include?(User.current)
   end
 
   def can_create_comments?(container)
     admin? ||
     meeting_manager? ||
     container.users.include?(User.current) ||
-    container.watchers.include?(User.current)
+    @object.watchers.include?(User.current)
   end
 end
