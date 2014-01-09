df0c5b2691b108f70aa02765b0e0aff33142d9ef
rubynovich <roman@shipiev.me>
2014-01-09 11:37:35 +0400

    refactoring

--- app/models/meeting_protocol.rb
+++ app/models/meeting_protocol.rb
@@ -55,7 +55,7 @@ class MeetingProtocol < ActiveRecord::Base
 
   before_create :add_author_id
   after_save :add_new_users_from_answers
-  # after_save :add_new_contacts_from_answers
+  after_save :add_new_contacts_from_answers
   after_save :add_time_entry_to_invites
 
   scope :active, -> {
