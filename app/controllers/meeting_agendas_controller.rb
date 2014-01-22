137427d4396ff59b33e2c7aa68b80a9b98f07b50
Alexander Tkachev <teksisto@gmail.com>
2014-01-22 15:11:32 +0400

    add: agenda watchers

--- app/controllers/meeting_agendas_controller.rb
+++ app/controllers/meeting_agendas_controller.rb
@@ -18,9 +18,12 @@ class MeetingAgendasController < ApplicationController
   helper :meeting_external_approvers
   include MeetingExternalApproversHelper
   include ApplicationHelper
+  helper :meeting_watchers
+  include MeetingWatchersHelper
 
   def show
 #    (render_403; return false) unless can_show_agenda?(@object)
+    @watchers = @object.watchers
     respond_to do |format|
       format.pdf {
         filename = ("Povestka_%04d" % [@object.id]) + @object.meet_on.strftime("_%Y-%m-%d.pdf")
