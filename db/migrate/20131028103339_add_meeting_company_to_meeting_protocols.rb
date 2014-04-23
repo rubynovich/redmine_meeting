3dfdf2a9b1079fbcdb292fad6fc57f709d0c3b56
Maksim <max@koritskiy.ru>
2014-04-23 14:58:53 +0400

    fix

--- db/migrate/20131028103339_add_meeting_company_to_meeting_protocols.rb
+++ db/migrate/20131028103339_add_meeting_company_to_meeting_protocols.rb
@@ -1,10 +1,10 @@
 class AddMeetingCompanyToMeetingProtocols < ActiveRecord::Migration
   def up
-    add_column :meeting_protocols, :meeting_company_id, :integer
+    #add_column :meeting_protocols, :meeting_company_id, :integer
     MeetingProtocol.update_all(meeting_company_id: MeetingCompany.first)
   end
 
   def down
-    remove_column :meeting_protocols, :meeting_company_id
+    #remove_column :meeting_protocols, :meeting_company_id
   end
 end
