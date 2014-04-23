0cb8d793fca13b8ae394bc1d31283785c1842954
Maksim <max@koritskiy.ru>
2014-04-23 14:53:10 +0400

    fix migrate

--- db/migrate/20131023110632_add_asserter_to_meeting_protocols.rb
+++ db/migrate/20131023110632_add_asserter_to_meeting_protocols.rb
@@ -1,5 +1,5 @@
 class AddAsserterToMeetingProtocols < ActiveRecord::Migration
   def change
-    add_column :meeting_protocols, :asserter_id, :integer
+    add_column :meeting_protocols, :asserter_id, :integer rescue nil
   end
 end
