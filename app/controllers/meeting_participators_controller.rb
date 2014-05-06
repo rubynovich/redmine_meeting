50246cb8e7a1f7ca8f9b21640c2d543b0a8af158
Alexander Tkachev <teksisto@gmail.com>
2014-05-06 12:00:12 +0400

    refactoring: данные о присутствоваших пользователях вытаскиваются из сессии отдельным методом

--- app/controllers/meeting_participators_controller.rb
+++ app/controllers/meeting_participators_controller.rb
@@ -19,9 +19,9 @@ class MeetingParticipatorsController < ApplicationController
 
   def create
     user_ids = (params[model_sym].present? ? params[model_sym][:user_ids] : [])
-    Rails.logger.error(" in Participator create".red)
+    Rails.logger.debug(" in Participator create".red)
     if @object.present?
-      Rails.logger.error("object present, work with db".red)
+      Rails.logger.debug("object present, work with db".red)
       user_ids.each do |user_id|
         turned_off_user = MeetingParticipator.where(user_id: user_id, meeting_protocol_id: params[:meeting_protocol_id], attended: false).first
         if turned_off_user
@@ -32,10 +32,10 @@ class MeetingParticipatorsController < ApplicationController
       end
       @members = @object.users
     else
-      Rails.logger.error("there is no object working with session".red)
+      Rails.logger.debug("there is no object working with session".red)
       session[session_sym] = session[session_sym].merge( Hash[ user_ids.map{|user_id| [user_id.to_i, true] }])
-      Rails.logger.error("hash in session after modificatipon".red + session[session_sym].inspect)
-      @members = User.sorted.find(session[session_sym].select{|k,v| v}.keys)
+      Rails.logger.debug("hash in session after modificatipon".red + session[session_sym].inspect)
+      @members = present_participators_from_session
     end
 
     respond_to do |format|
@@ -50,8 +50,8 @@ class MeetingParticipatorsController < ApplicationController
                  MeetingParticipator.where(model_sym_id => @object.id, user_id: params[:id]).each{|par| par.update_attribute(:attended, false) }
                  @object.users
                else
-                 Rails.logger.error('Participator destroy from session.'.red)
-                 Rails.logger.error('Before: '.red + session[session_sym].inspect)
+                 Rails.logger.debug('Participator destroy from session.'.red)
+                 Rails.logger.debug('Before: '.red + session[session_sym].inspect)
 
                  if session[:permanent_participators_ids].include?(params[:id].to_i)
                    session[session_sym][params[:id].to_i] = false
@@ -59,8 +59,8 @@ class MeetingParticipatorsController < ApplicationController
                    session[session_sym].delete(params[:id].to_i)
                  end
 
-                 Rails.logger.error('After: '.red + session[session_sym].inspect)
-                 User.sorted.find(session[session_sym].select{|k,v| v}.keys)
+                 Rails.logger.debug('After: '.red + session[session_sym].inspect)
+                 present_participators_from_session
                end
     
     respond_to do |format|
@@ -72,7 +72,7 @@ class MeetingParticipatorsController < ApplicationController
     @members = if @object.present?
                  @object.meeting_participators.where(attended: true).map(&:user)
                else
-                 User.sorted.find(session[session_sym].select{|k,v| v}.keys)
+                 present_participators_from_session
                end
 
     @no_members = User.active.like(params[:q]).sorted - @members
@@ -81,16 +81,6 @@ class MeetingParticipatorsController < ApplicationController
   end
 
   def accept
-#    @member = MeetingParticipator.find(params[:id])
-#    (render_403; return false) unless can_accept?(@member)
-#    solved_status_id = Setting.plugin_redmine_meeting[:solved_issue_status]
-#    issue = @member.issue
-#    issue.status_id = solved_status_id
-#    if issue.save
-#      flash[:notice] = l(:notice_successful_notice_accept)
-#    else
-#      flash[:error] = l(:error_notice_accept_failed)
-#    end
     @object = MeetingParticipator.find(params[:id])
     if @object.present? && @object.sended_notice_on.present? && @object.saw_protocol_on.blank?
       @object.saw_protocol_on = Time.now
@@ -103,6 +93,10 @@ class MeetingParticipatorsController < ApplicationController
   end
 
 private
+
+  def present_participators_from_session
+    User.sorted.find(session[session_sym].select{|k,v| v}.keys)
+  end
 
   def find_object
     @object = model_class.find(params[model_sym_id]) rescue nil
