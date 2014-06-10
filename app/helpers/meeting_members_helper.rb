e27fd8a37c334189f2cc47b8d0eeda4b709d467e
Maksim <max@koritskiy.ru>
2014-06-10 15:57:26 +0400

    fix

--- app/helpers/meeting_members_helper.rb
+++ app/helpers/meeting_members_helper.rb
@@ -13,6 +13,11 @@ module MeetingMembersHelper
     if member.present? && member.issue.present?
       link_to(member.issue.status, controller: 'issues', action: 'show', id: member.issue_id)
     else
+      if Redmine::Plugin.all.map(&:id).include?(:redmine_vacation)
+        if vacation_range = member.meeting_agenda.member_on_vacation?(member.user)
+          return l(:label_meeting_member_was_not_invite_but_on_vacation, {:from => vacation_range.start_date.strftime("%d.%m.%Y"), :to => vacation_range.end_date.strftime("%d.%m.%Y")})
+        end
+      end
       l(:label_meeting_member_was_not_invite)
     end
   end
