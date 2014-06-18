8d73074d613c87f4e2c4953d6cbb6cdbf8b91455
Maksim <max@koritskiy.ru>
2014-06-18 11:40:41 +0400

    добавлен select2 для докладчиков совещаний

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,1 +1,9 @@
 //= require ./jquery_nested_form
+
+$(document).on('nested:fieldAdded', function(){
+    if (! isMobile()) {
+        var user_select = $('select[id$=user_id]');
+        user_select.attr('style','width:300px; min-width: 300px;');
+        user_select.select2();
+    }
+})
